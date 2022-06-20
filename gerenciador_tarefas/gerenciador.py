d0912440271b8a35b5dad2f45a2b2a625c7be8ce
odinSanliveira <odin.sanliveira@gmail.com>
2022-06-19 21:11:09 -0300

    detalhes de tarefas funcionando. atualização de tarefas (WIP)

--- gerenciador_tarefas/gerenciador.py
+++ gerenciador_tarefas/gerenciador.py
@@ -1,14 +1,16 @@
-import json
 from uuid import UUID, uuid4
 from fastapi import FastAPI
+import uvicorn
 from pydantic import BaseModel, constr
 from fastapi.encoders import jsonable_encoder
 
 app = FastAPI()
+
 class TarefaEntrada(BaseModel):
     titulo: constr(min_length=3, max_length=50)
     descrição: constr(max_length=140)
     estado: str
+
 class Tarefa(TarefaEntrada):
     id: UUID
 
@@ -29,21 +31,36 @@ TAREFAS = [{
         "titulo": "lavar roupas",
         "descrição": "estão sujas",
         "estado": "não finalizado",
-    }]
+    },
+    {
+        "id": "3fa85f64-5717-4562-b3fc-2c963f66af22",
+        "titulo": "lavar roupas",
+        "descrição": "estão sujas",
+        "estado": "não finalizado",
+    }
+    
+    ]
 
 
 @app.get("/tarefas")
 def listar():
     return TAREFAS
 
+@app.get("/tarefas/{id}")
+def listar_detalhes(id:str) -> dict:
+    # tarefa = {"id": id}
+    resultado = [tarefa for tarefa in TAREFAS if tarefa['id'] == id]
+    if resultado:
+        return resultado[0]
+
 @app.post('/tarefas', response_model=Tarefa)
 def criar(tarefa: TarefaEntrada):
     nova_tarefa = tarefa.dict()
     nova_tarefa.update({"id": uuid4()})
+    TAREFAS.append(nova_tarefa)
     return nova_tarefa
 
 @app.put("/tarefas/{id}", response_model=Tarefa)
-def atualizar_estado(id: str, tarefa: Tarefa):
-    atualizar_tarefa_encoded = jsonable_encoder(id)
-    tarefa[id] = atualizar_tarefa_encoded
-    return atualizar_tarefa_encoded
+def atualizar_estado(id: str, tarefa: TarefaEntrada):
+    TAREFAS[tarefa.id] = tarefa
+    return tarefa
